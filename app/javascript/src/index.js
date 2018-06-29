bddc34c023ffd997418ded5c5f15755cfe3bdb28
ian <ian@nervos.org>
2018-06-29 19:25:36 +0800

    seutp vue.js

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -1,4 +1,3 @@
-import Turbolinks from "turbolinks";
 import Rails from "rails-ujs";
 import * as Web3 from "@nervos/web3";
 
@@ -19,5 +18,17 @@ import "./stylesheets/application.scss";
 // Src
 import "./images/index";
 
+import Vue from 'vue'
+import App from '../app.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const el = document.body.appendChild(document.createElement('hello'))
+  const app = new Vue({
+    el,
+    render: h => h(App)
+  })
+
+  console.log(app)
+})
+
 Rails.start();
-Turbolinks.start();
