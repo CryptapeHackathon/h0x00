cb6f6ac3fa0f93b7e78363528bf5433d6aabcbbc
ian <ian@nervos.org>
2018-06-29 15:25:41 +0800

    import web3.js

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -1,15 +1,23 @@
 import Turbolinks from "turbolinks";
 import Rails from "rails-ujs";
+import * as Web3 from "@nervos/web3";
+
+window.Web3 = Web3;
+
+// Example
+let web3 = Web3.default("http://47.94.105.230:1337");
+web3.cita.getMetaData().then(function(res) {
+  console.log(res);
+});
 
 // Bootstrap
-import 'bootstrap/dist/js/bootstrap';
+import "bootstrap/dist/js/bootstrap";
 
 // Scss
-import './stylesheets/application.scss';
+import "./stylesheets/application.scss";
 
 // Src
-import './images/index';
-
+import "./images/index";
 
 Rails.start();
 Turbolinks.start();
