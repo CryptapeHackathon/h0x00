e41401aacebdbd8689b97c6e94be70c6547580d1
ian <ian@nervos.org>
2018-06-29 21:24:11 +0800

    cleanup debug code

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -3,12 +3,6 @@ import * as Web3 from "@nervos/web3";
 
 window.Web3 = Web3;
 
-// Example
-let web3 = Web3.default("http://47.94.105.230:1337");
-web3.cita.getMetaData().then(function(res) {
-  console.log(res);
-});
-
 // Bootstrap
 import "bootstrap/dist/js/bootstrap";
 
