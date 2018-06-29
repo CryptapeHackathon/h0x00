2291a26dbb08d882d60e9b7848b2d6bf2349fb8e
ian <ian@nervos.org>
2018-06-30 02:45:12 +0800

    Add --address param to send transaction

--- scripts/transferAnyERC20Token.sh
+++ scripts/transferAnyERC20Token.sh
@@ -8,4 +8,4 @@ mkdir -p "$HOME/.cita-cli"
 eval "data=$(cita-cli ethabi encode function contracts/tokens_sol_FixedSupplyToken.abi transferAnyERC20Token --param $ADDR --param $VAL)"
 echo $data
 
-cita-cli rpc sendRawTransaction --private-key $PK --code "0x$data"
+cita-cli rpc sendRawTransaction --private-key $PK --code "0x$data" --address "$ERC20" --chain-id 1
