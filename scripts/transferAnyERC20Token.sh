c7ae545af03ccca6629e629d81fbe9527c679f14
ian <ian@nervos.org>
2018-06-30 02:24:36 +0800

    script: transferAnyERC20Token

---
+++ scripts/transferAnyERC20Token.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -u
+[ -n "${DEBUG:-}" ] && set -x || true
+
+mkdir -p "$HOME/.cita-cli"
+eval "data=$(cita-cli ethabi encode function contracts/tokens_sol_FixedSupplyToken.abi transferAnyERC20Token --param $ADDR --param $VAL)"
+echo $data
+
+cita-cli rpc sendRawTransaction --private-key $PK --code "0x$data"
