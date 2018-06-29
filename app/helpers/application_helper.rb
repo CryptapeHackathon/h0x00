0b6537a46cbde057849ceacaddff948316f7e95a
classicalliu <classicalliu@gmail.com>
2018-06-29 18:51:43 +0800

    footer click and active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def list?
+    params[:controller] == "chains" && params[:action] == 'index'
+  end
+
+  def sell?
+    params[:controller] == "orders" && params[:action] == "sell"
+  end
 end
