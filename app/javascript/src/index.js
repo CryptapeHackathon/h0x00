3d06f77f322ca784f6296961e8677f92d3de7908
classicalliu <classicalliu@gmail.com>
2018-06-30 10:18:33 +0800

    remove vue

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -13,17 +13,17 @@ import "./stylesheets/application.scss";
 import "./images/index";
 import './sweet';
 
-import Vue from 'vue'
-import App from '../app.vue'
-
-document.addEventListener('DOMContentLoaded', () => {
-  const el = document.body.appendChild(document.createElement('hello'))
-  const app = new Vue({
-    el,
-    render: h => h(App)
-  })
-
-  console.log(app)
-})
+// import Vue from 'vue'
+// import App from '../app.vue'
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   const el = document.body.appendChild(document.createElement('hello'))
+//   const app = new Vue({
+//     el,
+//     render: h => h(App)
+//   })
+//
+//   console.log(app)
+// })
 
 Rails.start();
